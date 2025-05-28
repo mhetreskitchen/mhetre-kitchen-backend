915ed54d7c70ce95321c7d361944932cafa3efa6
mhetreskitchen <mhetreskitchen@gmail.com>
2025-05-28 21:16:33 +0800

    Initial commit

---
+++ src/test/java/com/mhetre/kitchen/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhetre.kitchen.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
